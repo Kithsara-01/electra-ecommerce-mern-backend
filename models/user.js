50e3735f63d8ffa9f05d0cf189cf14ff8198ffce
Kithsara-01 <studymail494@gmail.com>
2026-07-05 12:09:38 +0530

    do some upadtes in authentication✏️

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema(
     email:{
         type:String,
         required:true,
-        unique:true,
+        unique:true, // this ensures that no two users can have the same email address
         trim:true,
         lowercase:true
     },
