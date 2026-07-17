9db37a71dc1210c95dce2f5f9492fbaceea445b2
Kithsara-01 <studymail494@gmail.com>
2026-07-18 00:25:32 +0530

    overall functionalities are completed in admin dashbord🥲✏️
    reamining only extra  improvemnts

--- models/user.js
+++ models/user.js
@@ -44,7 +44,7 @@ const userSchema = new mongoose.Schema(
 
     role:{
         type:String,
-        enum:["Admin","Customer","Supplier"],
+        enum:["Admin","Customer"],
         default:"Customer"
     },
 
