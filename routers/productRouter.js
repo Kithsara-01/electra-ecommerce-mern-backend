499daf0bbdc298bbb7a2d58498979a16d31d7657
Kithsara-01 <studymail494@gmail.com>
2026-07-24 16:30:53 +0530

    Created AI feature to generate description and alternative names for admin-addproduct page by using prod-name and category✏️✏️

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -2,12 +2,14 @@ import express from "express";
 
 import {
   createProduct,
-  deleteProduct,
   getAllProducts,
   getProductById,
-  searchProducts,
   updateProduct,
   updateProductStock,
+  deleteProduct,
+  searchProducts,
+  generateAIProductDescription,
+  generateAIAlternativeNames,
 } from "../controllers/productController.js";
 
 import { protect } from "../middlewares/authMiddleware.js";
@@ -22,6 +24,10 @@ productRouter.get("/search/:query", searchProducts);
 // Admin Routes
 productRouter.get("/admin", protect, authorize("Admin"), getAllProducts);
 productRouter.post("/", protect, authorize("Admin"), createProduct);
+
+productRouter.post("/ai-description", protect, authorize("Admin"), generateAIProductDescription);
+productRouter.post( "/ai-alternative-names", protect, authorize("Admin"), generateAIAlternativeNames);
+
 productRouter.put("/:productId", protect, authorize("Admin"), updateProduct);
 productRouter.patch("/:productId/stock", protect, authorize("Admin"), updateProductStock);
 productRouter.delete("/:productId", protect, authorize("Admin"), deleteProduct);
